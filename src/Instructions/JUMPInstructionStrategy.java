f5048209c605d4ded7686af0abb83aa2adfd7d77
ashwor11 <75903644+ashwor11@users.noreply.github.com>
2023-11-29 17:27:52 +0300

    init3

---
+++ src/Instructions/JUMPInstructionStrategy.java
@@ -0,0 +1,11 @@
+package Instructions;
+
+import java.util.ArrayList;
+
+public class JUMPInstructionStrategy implements IInstructionStrategy {
+
+    @Override
+    public String GenerateBinaryInstruction(ArrayList<String> elements) {
+        Helper
+    }
+}
